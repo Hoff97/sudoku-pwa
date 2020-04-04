387586f5944bf7347810e50b1ff33d229b53c6e3
Frithjof Winkelmann <frithjof97@web.de>
2020-04-04 13:19:56 +0200

    Add notes to sudoku component

--- website/src/components/sudoku/sudoku.tsx
+++ website/src/components/sudoku/sudoku.tsx
@@ -13,6 +13,8 @@ type CellValue = number | undefined;
 interface Cell {
     value: CellValue;
     wrong: boolean;
+    editable: boolean;
+    notes: number[];
 }
 
 interface SudokuState {
@@ -20,7 +22,35 @@ interface SudokuState {
     focus: number[];
 }
 
+interface SudokuHelper {
+    generate_sudoku(): string;
+    solve_sudoku(s: string): string;
+}
+
+function fromString(str: string): Cell[][] {
+    const cells: Cell[][] = [];
+    for (let i = 0; i < 9; i++) {
+        const row: Cell[] = [];
+        for (let j = 0; j < 9; j++) {
+            const val = str[i*9 + j];
+
+            row.push({
+                value: val === '.' ? undefined : parseInt(val),
+                wrong: false,
+                editable: val === '.',
+                notes: []
+            });
+        }
+        cells.push(row);
+    }
+
+    return cells;
+}
+
 export class Sudoku extends Component<{}, SudokuState> {
+    private sudokuHelper?: SudokuHelper;
+    private ctrlPressed = false;
+
     constructor(props: any, context: any) {
         super(props, context);
 
@@ -30,7 +60,9 @@ export class Sudoku extends Component<{}, SudokuState> {
             for (let j = 0; j < 9; j++) {
                 row.push({
                     value: undefined,
-                    wrong: false
+                    wrong: false,
+                    editable: true,
+                    notes: []
                 });
             }
             cells.push(row);
@@ -40,6 +72,22 @@ export class Sudoku extends Component<{}, SudokuState> {
             cells: cells,
             focus: [-1,-1]
         };
+
+        import("wasm-game-of-life").then(module => {
+            this.sudokuHelper = module;
+
+            this.generateSudoku();
+        });
+    }
+
+    generateSudoku() {
+        if (this.sudokuHelper) {
+            const sudoku = this.sudokuHelper.generate_sudoku();
+            const cells = fromString(sudoku);
+            this.setState({
+                cells
+            });
+        }
     }
 
     focus(x: number, y: number) {
@@ -51,9 +99,41 @@ export class Sudoku extends Component<{}, SudokuState> {
 
     setCell(x: number, y: number, value: CellValue) {
         const cells = this.state.cells;
-        cells[x][y].value = value;
+        if (cells[x][y].editable) {
+            cells[x][y].value = value;
 
-        this.check(cells);
+            this.check(cells);
+        }
+    }
+
+    setNote(x: number, y: number, value: number) {
+        const cells = this.state.cells;
+
+        if (cells[x][y].editable) {
+            const ix = cells[x][y].notes.indexOf(value);
+            if (ix != -1) {
+                cells[x][y].notes.splice(ix, 1);
+            } else {
+                cells[x][y].notes.push(value);
+                cells[x][y].notes = cells[x][y].notes.sort();
+            }
+
+            this.setState({
+                cells
+            });
+        }
+    }
+
+    deleteNotes(x: number, y: number) {
+        const cells = this.state.cells;
+
+        if (cells[x][y].editable) {
+            cells[x][y].notes = [];
+
+            this.setState({
+                cells
+            });
+        }
     }
 
     check(cells: Cell[][]) {
@@ -147,9 +227,17 @@ export class Sudoku extends Component<{}, SudokuState> {
             } else if (event.keyCode == 40) {
                 this.focus(xFocus + 1, yFocus);
             } else if (parsed != NaN && parsed >= 1 && parsed <= 9) {
-                this.setCell(xFocus, yFocus, parsed);
+                if (event.ctrlKey) {
+                    this.setNote(xFocus, yFocus, parsed);
+                } else {
+                    this.setCell(xFocus, yFocus, parsed);
+                }
             } else if (event.key === 'Backspace') {
-                this.setCell(xFocus, yFocus, undefined);
+                if (event.ctrlKey) {
+                    this.deleteNotes(xFocus, yFocus);
+                } else {
+                    this.setCell(xFocus, yFocus, undefined);
+                }
             }
         }
     }
@@ -159,22 +247,35 @@ export class Sudoku extends Component<{}, SudokuState> {
 
         return <div className={style.sudoku}>
             <table>
-                {this.state.cells.map((row, x) => 
+                {this.state.cells.map((row, x) =>
                     <tr>
-                        {row.map((cell, y) => {
-                            const focused = xFocus == x && yFocus == y ? style.focus : '';
-                            const wrong = cell.wrong ? style.wrong : '';
-                            const classes = `${style.cell} ${focused} ${wrong}`
-
-                            return <td className={classes}
-                                onClick={() => this.focus(x,y)}
-                                onKeyPress={event => this.handleKeyPress(event)}>
-                                    {cell.value}
-                            </td>;
-                        })}
+                        {row.map((cell, y) => this.renderCell(cell, x, y))}
                     </tr>
                 )}
             </table>
+            <button onClick={() => this.generateSudoku()}>Generate new sudoku</button>
         </div>;
+    }
+
+    renderCell(cell: Cell, x: number, y: number): ComponentChild {
+        let [xFocus, yFocus] = this.state.focus;
+
+        const focused = xFocus == x && yFocus == y ? style.focus : '';
+        const wrong = cell.wrong ? style.wrong : '';
+        const set = !cell.wrong && cell.editable ? style.set : '';
+        const classes = `${style.cell} ${focused} ${wrong} ${set}`
+
+        let content;
+        if (cell.value) {
+            content = cell.value;
+        } else {
+            content = <span className={style.note}>{cell.notes.join('')}</span>
+        }
+
+        return <td className={classes}
+            onClick={() => this.focus(x,y)}
+            onKeyPress={event => this.handleKeyPress(event)}>
+                {content}
+        </td>;
     }
 }
