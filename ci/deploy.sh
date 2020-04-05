075736806741e2be7b5f3c0d53c96920adeae0a7
Frithjof Winkelmann <frithjof97@web.de>
2020-04-05 10:36:32 +0200

    Change git origin for deployment

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,13 @@
 cd ./website/
 
+git remote remove origin
+git remote add origin git@github.com:Hoff97/sudoku-pwa.git
+
+git branch --set-upstream-to origin master
+git checkout gh-pages
+git branch --set-upstream-to origin gh-pages
+git checkout master
+
 # Has to be set for now, because wasm can only be imported as a critical
 # dependency.
 export CI=false
