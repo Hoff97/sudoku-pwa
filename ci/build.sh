008592a690af532d4e74112cb8ef4befa36cbacb
Frithjof Winkelmann <frithjof97@web.de>
2020-04-05 10:20:56 +0200

    Fix CI build

--- ci/build.sh
+++ ci/build.sh
@@ -3,4 +3,9 @@ wasm-pack build
 
 cd ../website/
 npm install
+
+# Has to be set for now, because wasm can only be imported as a critical
+# dependency.
+export CI=false
 npm run build
+export CI=true
