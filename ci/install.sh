108cc7dcbd490ec99cfbd297c307fdf7b26d6947
Frithjof Winkelmann <frithjof97@web.de>
2020-04-05 10:55:05 +0200

    Set up ssh key

--- ci/install.sh
+++ ci/install.sh
@@ -2,4 +2,6 @@ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 eval `ssh-agent -s`
 chmod 400 ./deploy_key
-ssh-add ./deploy_key
+cp ./deploy_key ~/.ssh/deploy_key
+ssh-add ~/.ssh/deploy_key
+echo "IdentityFile ~/.ssh/deploy_key" > ~/.ssh/config
